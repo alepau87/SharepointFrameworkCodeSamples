d72bb2e34e7768f745ea968852396a5867c14211
levalencia <luise.valencia@gmail.com>
2016-10-15 18:20:47 -0500

    changed name

---
+++ Datatables/src/webparts/pnpcrudsample/MockHttpClient.ts
@@ -0,0 +1,16 @@
+import { IListItem } from './PnpcrudSampleWebPart';
+
+export default class MockHttpClient {
+//Title,h7vv,v7nw,mczsId,mczsStringId,BooleanColumn
+    private static _items: IListItem[] =
+    [
+      { Title: 'Mock List', h7vv: '1',v7nw :'01-01-2016',mczsId:'Luis Esteban Valencia',BooleanColumn:'Yes' },
+      { Title: 'Mock List2', h7vv: '1',v7nw :'01-01-2016',mczsId:'Luis Esteban Valencia',BooleanColumn:'Yes' },
+    ];
+
+    public static get(restUrl: string, options?: any): Promise<IListItem[]> {
+      return new Promise<IListItem[]>((resolve) => {
+              resolve(MockHttpClient._items);
+          });
+      }
+}
